a3da0ed304a9111958d4b915d2050d59cbee3768
Simon Hamblin <simon.hamblin@gmail.com>
2024-02-06 19:48:06 +1100

    update theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
+import theme from './theme/Index';
 
 import Nav from './components/Nav';
 
