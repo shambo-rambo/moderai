6fc342cc5acac8de54f433e7750a9bb9dcea57c2
Simon Hamblin <simon.hamblin@gmail.com>
2024-02-06 21:17:06 +1100

    vite 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,20 +14,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  build: {
-    outDir: 'build', // Specify the build output directory
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              // Node modules are split into a separate chunk
-              return 'vendor';
-            }
-          }
-        }
-      }
-    }
-  },
-});
+  }
+})
